6fb8a7ae4729b574f4170114f93409f3aaa660b3
afriansyoga <afrians512@gmail.com>
2024-07-15 11:11:51 +0700

    yoga: models

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -4,8 +4,26 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Order extends Model
 {
-    use HasFactory;
+    use HasFactory,SoftDeletes;
+    
+    protected $fillable = [
+        'id_user',
+        'id_product',
+        'price',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'id_user');
+    }
+    
+    public function product()
+    {
+        return $this->belongsTo(Product::class, 'id_product');
+    }
+
 }
--- app/Models/Product.php
+++ app/Models/Product.php
@@ -4,8 +4,16 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Product extends Model
 {
-    use HasFactory;
+    use HasFactory,SoftDeletes;
+
+    protected $fillable = [
+        'product_name',
+        'description',
+        'category',
+        'price',
+    ];
 }
